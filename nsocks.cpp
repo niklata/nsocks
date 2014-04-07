346e72ad27a0423526e6ed6849425c16ed48c2b5
Nicholas J. Kain <nicholas@kain.us>
2014-04-07 19:22:56 -0400

    Update to the latest ncmlib.

--- nsocks.cpp
+++ nsocks.cpp
@@ -434,7 +434,7 @@ static void process_options(int ac, char *av[])
     if (chroot_path.size())
         nk_set_chroot(chroot_path.c_str());
     if (nsocks_uid || nsocks_gid)
-        nk_set_uidgid(nsocks_uid, nsocks_gid, NULL);
+        nk_set_uidgid(nsocks_uid, nsocks_gid, NULL, 0);
 
     // if (enforce_seccomp())
     //     log_line("seccomp filter cannot be installed");
